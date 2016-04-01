3059e2200bf1c8a76c0cab66058c58590101e676
Peter <email": null>
2016-04-01 12:00:19 -0400

    Add message routes and controller.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,7 @@
 class MessagesController < ApplicationController
+
+def new
+  @message = Message.new
+end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   resources :welcome, only: [:index]
   resources :secrets
   resources :algorithms
+  resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
