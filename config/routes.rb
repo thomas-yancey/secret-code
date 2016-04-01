71b18a9e4abda7be7eb223a2c52e7a9af69d46dd
Peter <email": null>
2016-04-01 11:42:21 -0400

    Algorithms constroller generated

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   get 'users/:id' => 'users#show'
   resources :welcome, only: [:index]
   resources :secrets
+  resources :algorithms
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
