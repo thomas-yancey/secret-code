62875c226a9b5d98246b004d7fa33ff4212cdbf3
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-04-02 18:22:16 -0400

    Add some edits to rspec files.

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
-  factory :user do
-    sequence(:email) {|n| "email#{n}@gmail.com" }
-    password "password"
+  sequence :email do |n|
+    "person#{n}@example.com"
+  end
+end
+
+FactoryGirl.define do
+  factory :user, :class => 'User' do
+    email
+    password '12345678'
+    password_confirmation '12345678'
   end
 end
