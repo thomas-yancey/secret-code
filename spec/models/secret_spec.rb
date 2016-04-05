db6a2975f181c6c3de869b1156ba30f32344cdba
Jonathan Silvestri <silvestrijonathan@gmail.com>
2016-04-05 15:52:55 -0400

    Add test for secret pretty date

--- spec/models/secret_spec.rb
+++ spec/models/secret_spec.rb
@@ -16,4 +16,14 @@ describe Secret do
 
   end
 
+  context "methods" do
+    context "#pretty_date" do
+      it "should convert a secret's date to a human readable format" do
+        secret = FactoryGirl.create(:secret)
+        secret.created_at = secret.pretty_date
+        expect(secret.created_at).to eq(secret.pretty_date)
+      end
+    end
+  end
+
 end
